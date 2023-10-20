7d981ad394967e4b28c846f27b6458dbbe60d281
Numa Leone <numa.leone>
2023-10-20 10:47:26 -0300

    initial setup of libraries and colors

---
+++ src/pages/DesignSystem/index.tsx
@@ -0,0 +1,12 @@
+import Button from "../../components/Button";
+
+const DesignSystem = () => {
+  return (
+    <>
+      <Button variant={"outlined"}>Outlined</Button>
+      <Button variant={"fulfilled"}>Fullfiled</Button>
+    </>
+  );
+};
+
+export default DesignSystem;
