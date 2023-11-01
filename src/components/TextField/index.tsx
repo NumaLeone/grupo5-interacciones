edae4c01074186fe2e5ae84ff6d51ddbdca08934
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2023-11-01 01:42:18 -0300

    Storybook done 2

---
+++ src/components/TextField/index.tsx
@@ -0,0 +1,15 @@
+import HomeIcon from "../../icons/navbar/HomeIcon";
+import SearchIcon from "../../icons/navbar/SearchIcon";
+import CartIcon from "../../icons/navbar/CartIcon";
+import AccountIcon from "../../icons/navbar/AccountIcon";
+
+const TextField = ()=>{
+    return(<>
+        <HomeIcon checked={true}/>
+        <SearchIcon checked={true}/>
+        <CartIcon checked={true}/>
+        <AccountIcon checked={true}/>
+        </>)
+}
+
+export default TextField;
