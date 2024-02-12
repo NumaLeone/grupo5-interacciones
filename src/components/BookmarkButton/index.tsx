0ff3aed80b066aa504917a7f4b1b13a1759df637
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2024-02-11 23:29:21 -0300

    IngredientCard improved and BookmarkButton corrected

--- src/components/BookmarkButton/index.tsx
+++ src/components/BookmarkButton/index.tsx
@@ -16,8 +16,12 @@ const BookmarkButton = ({size=40}: BookmarkButtonProps) => {
     return (
         <>
             <button
-                className='p-1 rounded-full bg-greyscale-100'
+                className='flex items-center p-1 rounded-full bg-greyscale-100'
                 onClick={handleClick}
+                style={{
+                    width: 'fit-content',  // Se adapta al contenido en el eje X
+                    height: 'fit-content', // Se adapta al contenido en el eje Y
+                }}
             >
                 <BookmarkIcon checked={isClicked} size={size} color={"text-primary-500"}/>
             </button>
--- src/components/IngredientCard/index.tsx
+++ src/components/IngredientCard/index.tsx
@@ -5,28 +5,22 @@ interface IngredientCardInterface {
     name: string,
     price: number,
     pricePerKG: number,
-    saved?: boolean,
     img: string
 }
 
-const handleClick = () =>{
-    console.log(1)
-    return 1
-}
-
-const IngredientCard = ({name, price, pricePerKG, saved = false, img = img1}: IngredientCardInterface) => {
+const IngredientCard = ({name='name', price=0, pricePerKG=0, img = img1}: IngredientCardInterface) => {
 
     return (
         <>
-            <div className={"h-64 w-44 bg-secondary-10 rounded-2xl shadow-cardShadow p-4 flex flex-col"}>
+            <div className={"h-68 w-44 bg-secondary-10 rounded-2xl shadow-cardShadow p-4 flex flex-col"}>
                 <div className="flex h-36 w-36 mb-1.5 rounded-2xl bg-cover justify-end bg-center p-2" style={{ backgroundImage: `url(${img})` }} >
-                    <BookmarkButton size={32} checked={saved} onClick={handleClick}></BookmarkButton>
+                    <BookmarkButton size={32}></BookmarkButton>
                 </div>
                 <div className="font-SFProText text-ingredientName">{name}</div>
                 <div className={"mb-3 "}>$ {pricePerKG} per kg</div>
                 <div className="flex flex-row justify-between">
                     <div className={" mr-8 font-SFProText text-ingredientsCard"}>$ {price}</div>
-                    <AddToCartButton/>
+                    <AddToCartButton size={40}/>
                 </div>
             </div>
         </>
