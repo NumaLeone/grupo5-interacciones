b813005b3f2936ce2f0c3f68085cc50cb641c3ca
Numa Leone <numa.leone>
2024-02-13 13:09:45 -0300

    adds the cartitem components, icons and storybooks

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,41 +1,53 @@
-import {VariantProps, cva} from "class-variance-authority";
+import { VariantProps, cva } from "class-variance-authority";
 import React from "react";
 
 const typographyVariant = cva("text-base font-SFProText", {
-    variants: {
-        type: {
-            h1: ["text-4xl"],
-            h2: ["text-3xl"],
-            h3:["text-2xl"],
-            h4:["text-xl"],
-            body1: ["mb-2"],
-            body2: ["text-sm mb-2"],
-        },
-        weight: {
-            normal: ["font-normal"],
-            bold: ["font-bold"],
-            medium: ["font-medium"]
-        }
+  variants: {
+    type: {
+      h1: ["text-4xl"],
+      h2: ["text-3xl"],
+      h3: ["text-2xl"],
+      h4: ["text-xl"],
+      body1: ["text-base"],
+      body2: ["text-sm"],
     },
-    defaultVariants: {
-        type: "body1",
-        weight: "normal"
+    weight: {
+      normal: ["font-normal"],
+      bold: ["font-bold"],
+      medium: ["font-medium"],
     },
+  },
+  defaultVariants: {
+    type: "body1",
+    weight: "normal",
+  },
 });
 
 export interface TypographyProps
-    extends Omit<React.HTMLProps<HTMLDivElement>, 'type'>,
-        VariantProps<typeof typographyVariant> {
-    variantType: "h1" | "h2" | "h3" | "h4" | "body1" | "body2";
-    variantWeight: "normal" | "bold" | "medium";
+  extends Omit<React.HTMLProps<HTMLDivElement>, "type">,
+    VariantProps<typeof typographyVariant> {
+  variantType: "h1" | "h2" | "h3" | "h4" | "body1" | "body2";
+  variantWeight: "normal" | "bold" | "medium";
 }
 
-const Typography: React.FC<TypographyProps> = ({variantType, variantWeight, children, ...props}) => {
-    return (
-        <div className={typographyVariant({type: variantType, weight: variantWeight})} {...props}>
-            {children}
-        </div>
-    );
+const Typography: React.FC<TypographyProps> = ({
+  variantType,
+  variantWeight,
+  children,
+  className = "",
+  ...props
+}) => {
+  return (
+    <div
+      className={`${typographyVariant({
+        type: variantType,
+        weight: variantWeight,
+      })} ${className}`}
+      {...props}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Typography;
